69dc3f855583db2473a7fb2464e68c9aa84e0a8b
PranjalWalunj <pranjalwalunj132@gmail.com>
2025-01-21 15:39:11 +0530

    shayari

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('ag mathe shodh shayari lavkar!!');
+ res.send('कभी रूठ जाऊ तो मना लेना,
+गुस्से से कहूं तो दिल पर मत लेना, 
+कल क्या पता हम रहे या न रहे, 
+इसलिए जब भी मैं आपको मिलूं, 
+तो कभी समोसा, कभी पानी पूरी खिला देना।');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
