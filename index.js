5d4970fe865af03079a5f0a4ee1a9beeddd962b5
PranjalWalunj <pranjalwalunj132@gmail.com>
2025-01-21 15:51:49 +0530

    zhala

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('If you ever get angry, take it for granted.
-Gusse se kahun to dil par mat lena, 
-Kal kya pata hum rahe ya na rahe, 
-So whenever I meet you, 
-To kabhi samosa, kabhi pani puri khila dena.');
+ res.send('If you ever get angry, take it for granted.Gusse se kahun to dil par mat lena, Kal kya pata hum rahe ya na rahe, So whenever I meet you, To kabhi samosa, kabhi pani puri khila dena.');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
